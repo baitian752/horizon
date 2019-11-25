39dda703a4d9fd1ddce2104d0dcc20fa3263f726
baitian <919600234@qq.com>
2019-11-25 16:24:24 +0000

    Front End

--- openstack_dashboard/dashboards/project/static/dashboard/project/workflow/run-task/run-task-modal.controller.js
+++ openstack_dashboard/dashboards/project/static/dashboard/project/workflow/run-task/run-task-modal.controller.js
@@ -14,24 +14,25 @@
  * limitations under the License.
  */
 (function () {
-  'use strict';
-
-  angular
-    .module('horizon.dashboard.project.workflow.run-task')
-    .controller('RunTaskModalController', RunTaskModalController);
-
-  RunTaskModalController.$inject = [
-    'horizon.dashboard.project.workflow.run-task.modal.service'
-  ];
-
-  function RunTaskModalController(modalService) {
-    var ctrl = this;
-
-    ctrl.openRunTaskWizard = openRunTaskWizard;
-
-    function openRunTaskWizard(launchContext) {
-      modalService.open(launchContext);
+    'use strict';
+  
+    angular
+      .module('horizon.dashboard.project.workflow.run-task')
+      .controller('RunTaskModalController', RunTaskModalController);
+  
+    RunTaskModalController.$inject = [
+      'horizon.dashboard.project.workflow.run-task.modal.service'
+    ];
+  
+    function RunTaskModalController(modalService) {
+      var ctrl = this;
+  
+      ctrl.openRunTaskWizard = openRunTaskWizard;
+  
+      function openRunTaskWizard(launchContext) {
+        modalService.open(launchContext);
+      }
     }
-  }
-
-})();
+  
+  })();
+  
---
+++ openstack_dashboard/dashboards/project/static/dashboard/project/workflow/run-task/run-task-wizard.controller.js
@@ -0,0 +1,39 @@
+/*
+ *    (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function () {
+    'use strict';
+  
+    angular
+      .module('horizon.dashboard.project.workflow.run-task')
+      .controller('RunTaskWizardController', RunTaskWizardController);
+  
+    RunTaskWizardController.$inject = [
+      '$scope',
+      'RunTaskModel',
+      'horizon.dashboard.project.workflow.run-task.workflow'
+    ];
+  
+    function RunTaskWizardController($scope, RunTaskModel, RunTaskWorkflow) {
+      // Note: we set these attributes on the $scope so that the scope inheritance used all
+      // through the run task wizard continues to work.
+      $scope.workflow = RunTaskWorkflow;     // eslint-disable-line angular/controller-as
+      $scope.model = RunTaskModel;           // eslint-disable-line angular/controller-as
+      $scope.model.initialize();
+      $scope.submit = $scope.model.createInstance;  // eslint-disable-line angular/controller-as
+    }
+  
+  })();
+  
